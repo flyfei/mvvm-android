9a065075af241b4b1222f7e71e334b275de0d5ef
zhaotengfei <zhaotengfei@goyoo.com>
2016-06-02 12:49:25 +0800

    add data bing

--- Android/app/src/main/java/com/tovi/mvvm/MainActivity.java
+++ Android/app/src/main/java/com/tovi/mvvm/MainActivity.java
@@ -1,13 +1,19 @@
 package com.tovi.mvvm;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tovi.mvvm.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User("Test", 12);
+        binding.setUser(user);
     }
 }
---
+++ Android/app/src/main/java/com/tovi/mvvm/User.java
@@ -0,0 +1,14 @@
+package com.tovi.mvvm;
+
+/**
+ * @author <a href='mailto:zhaotengfei9@gmail.com'>Tengfei Zhao</a>
+ */
+public class User {
+    public String name;
+    public int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
